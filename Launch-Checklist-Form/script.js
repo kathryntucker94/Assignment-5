d232d14923c154817f89861b33e901e779ad9a64
Kathryn Tucker <kathryn.tucker94@gmail.com>
2020-07-22 19:32:46 -0500

    Added the json elements to the form

--- Launch-Checklist-Form/script.js
+++ Launch-Checklist-Form/script.js
@@ -1,18 +1,33 @@
 window.addEventListener("load", function(){
-  let form = document.getElementById("launchForm");
-  form.addEventListener("submit", function(event){
-     let pilotName=document.querySelector("input[name=pilotName]");
-     let copilotName=document.querySelector("input[name=copilotName]");
-     let fuelLevel=document.querySelector("input[name=fuelLevel]");
-     let cargoMass=document.querySelector("input[name=cargoMass]");
-     let faultyItems=document.getElementById("faultyItems");
-     let pilotStatus=document.getElementById("pilotStatus");
-     let copilotStatus=document.getElementById("copilotStatus");
-     let fuelStatus=document.getElementById("fuelStatus");
-     let cargoStatus=document.getElementById("cargoStatus");
-     let launchStatus=document.getElementById("launchStatus");
+   let pilotName=document.querySelector("input[name=pilotName]");
+   let copilotName=document.querySelector("input[name=copilotName]");
+   let fuelLevel=document.querySelector("input[name=fuelLevel]");
+   let cargoMass=document.querySelector("input[name=cargoMass]");
+   let faultyItems=document.getElementById("faultyItems");
+   let pilotStatus=document.getElementById("pilotStatus");
+   let copilotStatus=document.getElementById("copilotStatus");
+   let fuelStatus=document.getElementById("fuelStatus");
+   let cargoStatus=document.getElementById("cargoStatus");
+   let launchStatus=document.getElementById("launchStatus");
+   let missionTarget=document.getElementById("missionTarget")
+   let form = document.getElementById("launchForm");
+   
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json){
+         missionTarget.innerHTML= `
+         <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[0].name}</li>
+               <li>Diameter: ${json[0].diameter}</li>
+               <li>Star: ${json[0].star}</li>
+               <li>Distance from Earth: ${json[0].distance} </li>
+               <li>Number of Moons: ${json[0].moons}</li>
+            <img src="${json[0].image}">
+            </ol>
+  `
+   form.addEventListener("submit", function(event){
 
-     if(pilotName.value ==="" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
+      if(pilotName.value ==="" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
          alert("All fields are required!");
          event.preventDefault()
       }else if(isNaN(pilotName.value) === false || isNaN(copilotName.value) === false || isNaN(Number(fuelLevel.value)) === true || isNaN(Number(cargoMass.value)) === true){
@@ -42,17 +57,9 @@ window.addEventListener("load", function(){
          copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready for launch.`;
          event.preventDefault();
       };
+   });
   });
 });
+})
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
